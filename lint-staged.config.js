87f33be810b2ddd49fbaf4b8db23549c79047e43
ngocduong <duong2lophot@gmail.com>
2025-02-05 00:01:09 +0700

    second commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    // eslint --fix:  kiểm tra và sửi lỗi code
-    // prettier --write : Format code tự động
-    '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write'],
-  }
+  '**/*.{js,jsx,ts,tsx}': ['eslint --fix', 'prettier --write', 'git add'],
+};
