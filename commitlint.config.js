e0af9d145b0d670cb5e99a94eb8a24099c9b666f
ngocduong <duong2lophot@gmail.com>
2025-02-09 10:04:27 +0700

    refactor: Commit eslint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-empty': [2, 'always', 'Infinity'],
+    'type-empty': [2, 'never'],
     'type-enum': [
       2,
       'always',
