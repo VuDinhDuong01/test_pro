e8d359638b07c859aa1312ec5d012cef0161232c
ngocduong <duong2lophot@gmail.com>
2025-02-09 10:00:07 +0700

    fix: Sửa lỗi đăng nhập khi nhập sai mật khẩu

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    // 'type-empty': [2, 'always', 0],
+    'type-empty': [2, 'always', 'Infinity'],
     'type-enum': [
       2,
       'always',
